8f2d283ce79002d216f5d05face325eca442e515
AndrewJGaut <ajg@umail.ucsb.edu>
2018-06-17 19:56:44 -0700

    added text file

--- Preprocessing/get_freq_data/scraping.y.py
+++
@@ -1,9 +0,0 @@
-import urllib as url
-
-from bs4 import BeautifulSoup
-
-page1 = url.open('https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/PG/2006/04/1-10000')
-
-soup1 = BeautifulSoup(page1)
-
-print(soup1.prettify)
